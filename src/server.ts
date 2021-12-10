d494c2b1d6bf6f1cbc3407b6af2f8207f6c9be00
Luccas Specht <luccas.specht70@gmail.com>
2021-12-10 11:37:37 -0300

    initial: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,7 @@ const app = express()
 app.listen(3333, () => {
   console.log('Server startes in port: 3333🚀')
 })
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello world' })
+})
