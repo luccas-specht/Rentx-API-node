a4cab2507d7b66d38ed9234c2630eef964254818
Luccas Specht <luccasspecht70@gmail.com>
2022-02-18 16:15:56 -0300

    add ManyToMany into car

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -3,12 +3,14 @@ import {
   CreateDateColumn,
   Entity,
   JoinColumn,
+  JoinTable,
+  ManyToMany,
   ManyToOne,
   PrimaryColumn,
 } from 'typeorm'
 import { v4 as uuidV4 } from 'uuid'
 
-import { Category } from '.'
+import { Category, Specification } from '.'
 
 @Entity('cars')
 export class Car {
@@ -45,6 +47,14 @@ export class Car {
 
   @Column()
   category_id: string
+
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: 'specification_cars',
+    joinColumn: { name: 'car_id' },
+    inverseJoinColumn: { name: 'specification_id' },
+  })
+  specifications: Specification[]
 
   constructor() {
     if (!this.id) {
