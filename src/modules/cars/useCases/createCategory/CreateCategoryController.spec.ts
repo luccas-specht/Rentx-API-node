8fd5add1f011a6b0dba947192a6794b7c9ab0c11
Luccas Specht <luccasspecht70@gmail.com>
2022-02-20 16:53:37 -0300

    feat(create category controller): clean up integration tests

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -46,4 +46,23 @@ describe('Create category controller', () => {
 
     expect(response.status).toBe(201)
   })
+
+  it('should not be able to create a new category with name exists', async () => {
+    const {
+      body: { token },
+    } = await request(app).post('/sessions').send({
+      email: 'fakeadmin@rentx.com.br',
+      password: 'fakeadmin',
+    })
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Fake category',
+        description: 'Fake description',
+      })
+      .set({ Authorization: `Bearer ${token}` })
+
+    expect(response.status).toBe(400)
+  })
 })
